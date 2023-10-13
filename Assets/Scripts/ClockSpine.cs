27a53ceb2ab6eede0fc2ae0ce65ab933346d596e
DESKTOP-JPPBER1\limhj <limhj2397@gmail.com>
2023-10-13 09:35:55 +0900

    시계 구현중

---
+++ Assets/Scripts/ClockSpine.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ClockSpine : MonoBehaviour
+{
+    private float speed = 1.6f;
+    private float timer;
+    public GameObject Clock;//xรเ
+
+    void Update()
+    {
+
+        timer += Time.deltaTime;
+
+
+
+    }
+}
